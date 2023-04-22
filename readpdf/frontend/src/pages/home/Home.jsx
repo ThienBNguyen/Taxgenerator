d81ddfa4e3bf5965c30ec479121fcd8044de8cae
ThienBNguyen <thien.nguyen30191@gmail.com>
2023-04-22 10:08:23 -0500

    application folder restructure, install redux and create schedule c form

--- readpdf/frontend/src/pages/home/Home.jsx
+++ readpdf/frontend/src/pages/home/Home.jsx
@@ -3,14 +3,12 @@ import About from '../about/About'
 import Header from '../../components/header/Header'
 import TaxForm from './TaxForm'
 import Footer from '../../components/footer/Footer'
-import Counter from '../../components/Counter'
 export default function Home() {
     return (
         <div>
             <Header></Header>
             <About />
             <TaxForm />
-            <Counter />
             <Footer></Footer>
         </div>
     )
